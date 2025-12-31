95d826445fa0b19c3e02a439e278443c8e11b81a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-12-30 23:30:02 -0300

    silverchain organize

---
+++ src/imports/imports.dep_declare.h
@@ -0,0 +1,4 @@
+#ifndef PROJECT_NAME_dep_declare
+#define PROJECT_NAME_dep_declare
+#include "../src_dependencies/dep_declare.dependencies.h"
+#endif
--- src/imports/imports.fdeclare.h
+++ src/imports/imports.fdeclare.h
@@ -1,3 +1,4 @@
+#include "imports.dep_declare.h"
 #ifndef PROJECT_NAME_fdeclare
 #define PROJECT_NAME_fdeclare
 #include "../fdeclare.all.h"
