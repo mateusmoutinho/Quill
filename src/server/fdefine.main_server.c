e2c325971507c3c14d8f18c37f5144ce7f0d0fef
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-02 00:00:00 -0300

    att

--- src/server/fdefine.main_server.c
+++ src/server/fdefine.main_server.c
@@ -4,6 +4,12 @@
 // silver_chain_scope_end
 
 CwebHttpResponse *main_server(CwebHttpRequest *request) {
+  if (dtw_starts_with(request->route, API_START)) {
+    return cweb_send_text("api area", 200);
+  }
+  if (dtw_starts_with(request->route, ADMIN_START)) {
+    return cweb_send_text("admin area", 200);
+  }
 
-  return cweb_send_text("Hello World", 200);
+  return cweb_send_text("execution area", 200);
 }
---
+++ src/server/routes/consts.routes.c
@@ -0,0 +1,7 @@
+//silver_chain_scope_start
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
+#include "../../imports/imports.dep_declare.h"
+//silver_chain_scope_end
+
+const char *API_START = "/api";
+const char *ADMIN_START = "/admin";
