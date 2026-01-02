3cb0e2f0dc8f21bd35019ee7213dc7cde495a361
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-02 00:59:56 -0300

    att

--- src/server/database/fdefine.users.c
+++ src/server/database/fdefine.users.c
@@ -11,3 +11,25 @@ DtwResource *find_user_by_name(const char *name) {
     
     return found_user;
 }
+DtwResource *find_user_by_email(const char *email) {
+    DtwResource *users = DtwResource_sub_resource(global_database, USERS_PATH);
+    DtwResource *found_user = DtwResource_find_by_primary_key_with_string(users, "email", email);
+    return found_user;
+}
+
+
+DtwResource *find_user_by_email_or_name(const char *email_or_name) {
+    DtwResource *users = DtwResource_sub_resource(global_database, USERS_PATH);
+    DtwResource *found_user = DtwResource_find_by_primary_key_with_string(users, "email", email_or_name);
+    if (found_user == NULL) {
+        found_user = DtwResource_find_by_primary_key_with_string(users, "name", email_or_name);
+    }
+    return found_user;
+}
+
+
+DtwResource *find_user_by_id(const char *id) {
+    DtwResource *users = DtwResource_sub_resource(global_database, USERS_PATH);
+    DtwResource *found_user = DtwResource_find_by_name_id(users, id);
+    return found_user;
+}   
