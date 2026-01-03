90703b9c8781d64adffc323dfcadebc18f3de547
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-03 05:33:47 -0300

    att

--- src/config/consts.errors_msg.c
+++ src/config/consts.errors_msg.c
@@ -9,6 +9,6 @@ const char *BODY_NOT_JSON = "body is not a json";
 const char *USERNAME_NOT_PROVIDED = "username not provided";
 const char *USER_ALREADY_EXISTS_MSG = "user already exists";
 const char *INVALID_USER_TYPE_MSG = "invalid user type, must be 'author', 'publisher', or 'root'";
-const char *KEY_NOT_FOUND_MSG = "key %s not found at path %s ";
-const char *KEY_WRONG_TYPE_MSG = "key %s at path %s is not of type %s";
+const char *KEY_NOT_FOUND_MSG = "key %s not found ";
+const char *KEY_WRONG_TYPE_MSG = "key %s is not of type %s";
 
